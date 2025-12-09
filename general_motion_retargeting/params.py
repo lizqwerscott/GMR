34a7e9c42faebbfe9db57a9562da57e27a1b801b
lizqwerscott <lizqwerscott@163.com>
2025-12-09 16:02:27 +0800

    feat: add Unitree G1 23-DOF robot models and retargeting config

--- general_motion_retargeting/params.py
+++ general_motion_retargeting/params.py
@@ -7,6 +7,7 @@ ASSET_ROOT = HERE / ".." / "assets"
 ROBOT_XML_DICT = {
     "unitree_g1": ASSET_ROOT / "unitree_g1" / "g1_mocap_29dof.xml",
     "unitree_g1_with_hands": ASSET_ROOT / "unitree_g1" / "g1_mocap_29dof_with_hands.xml",
+    "unitree_g1_23dof": ASSET_ROOT / "unitree_g1" / "g1_23dof_rev_1_0.xml",
     "unitree_h1": ASSET_ROOT / "unitree_h1" / "h1.xml",
     "unitree_h1_2": ASSET_ROOT / "unitree_h1_2" / "h1_2_handless.xml",
     "booster_t1": ASSET_ROOT / "booster_t1" / "T1_serial.xml",
@@ -29,6 +30,7 @@ IK_CONFIG_DICT = {
     "smplx":{
         "unitree_g1": IK_CONFIG_ROOT / "smplx_to_g1.json",
         "unitree_g1_with_hands": IK_CONFIG_ROOT / "smplx_to_g1.json",
+        "unitree_g1_23dof": IK_CONFIG_ROOT / "smplx_to_g1_23dof.json",
         "unitree_h1": IK_CONFIG_ROOT / "smplx_to_h1.json",
         "unitree_h1_2": IK_CONFIG_ROOT / "smplx_to_h1_2.json",
         "booster_t1": IK_CONFIG_ROOT / "smplx_to_t1.json",
@@ -73,6 +75,7 @@ IK_CONFIG_DICT = {
 ROBOT_BASE_DICT = {
     "unitree_g1": "pelvis",
     "unitree_g1_with_hands": "pelvis",
+    "unitree_g1_23dof": "pelvis",
     "unitree_h1": "pelvis",
     "unitree_h1_2": "pelvis",
     "booster_t1": "Waist",
@@ -93,6 +96,7 @@ ROBOT_BASE_DICT = {
 VIEWER_CAM_DISTANCE_DICT = {
     "unitree_g1": 2.0,
     "unitree_g1_with_hands": 2.0,
+    "unitree_g1_23dof": 2.0,
     "unitree_h1": 3.0,
     "unitree_h1_2": 3.0,
     "booster_t1": 2.0,
